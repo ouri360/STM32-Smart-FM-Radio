fba66476e7997bb77675f821a1467899ac80130e
Ouri <ourikoy@gmail.com>
2026-03-08 19:17:57 +0100

    Add .h files
    Core/Inc

---
+++ Inc/menu.h
@@ -0,0 +1,19 @@
+/*
+ * menu.h
+ *
+ *  Created on: Jan 21, 2026
+ *      Author: tromero
+ */
+
+#ifndef INC_MENU_H_
+#define INC_MENU_H_
+
+#include <stdint.h>
+
+extern uint8_t indice_menu_1;
+
+
+void menu(uint8_t page);
+
+
+#endif /* INC_MENU_H_ */
